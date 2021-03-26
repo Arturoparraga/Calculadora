4c1b5dd82387fae3f33b865d741c24af171efc35
Arturo Parraga <arturo@gmail.com>
2021-03-26 14:37:03 +0100

    Calculadora hasta el metodo restar

---
+++ Calcu.java
@@ -0,0 +1,25 @@
+public class Calcu
+{
+	float resultado;
+	
+	public Calcu() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+	public void pedirOperandos()
+	{
+		System.out.println("Por favor introduzca el primer operando");
+	}
+	
+	public float suma(float operando1, float operando2)
+	{
+		resultado = operando1 + operando2;
+		return resultado;
+	}
+
+	public float resta(float operando1, float operando2)
+	{
+		resultado = operando1 - operando2;
+		return resultado;
+	}
+}
