29b61f9b6677ac4ce5afc7ac938975ff9829a585
Arturo Parraga <arturo@gmail.com>
2021-03-28 19:56:27 +0200

    Añadido documentacion de javadoc

--- Calcu.java
+++ Calcu.java
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @author User
+ * @version 1.0
+ */
+
 public class Calcu
 {
 	float resultado;
@@ -10,23 +16,47 @@ public class Calcu
 	{
 		System.out.println("Por favor introduzca el primer operando");
 	}
+	/**
+	 * 
+	 * @param operando1 Valor que representa el primer operando
+	 * @param operando2 Valor que representa el segundo operando
+	 * @return devuelve un entero llamado resultado
+	 */
 	
 	public float suma(float operando1, float operando2)
 	{
 		resultado = operando1 + operando2;
 		return resultado;
 	}
+	/**
+	 * 
+	 * @param operando1 Valor que representa el primer operando
+	 * @param operando2 Valor que representa el segundo operando
+	 * @return devuelve un entero llamado resultado
+	 */
 
 	public float resta(float operando1, float operando2)
 	{
 		resultado = operando1 - operando2;
 		return resultado;
 	}
+	/**
+	 * 
+	 * @param operando1 Valor que representa el primer operando
+	 * @param operando2 Valor que representa el segundo operando
+	 * @return devuelve un entero llamado resultado
+	 */
 	public float multiplicacion(float operando1, float operando2)
 	{
 		resultado = operando1 * operando2;
 		return resultado;
 	}
+	/**
+	 * 
+	 * @param operando1 Valor que representa el primer operando
+	 * @param operando2 Valor que representa el segundo operando
+	 * @return devuelve un entero llamdo resultado
+	 */
 	
 	public float division(float operando1, float operando2)
 	{
@@ -41,6 +71,11 @@ public class Calcu
 			return -1;
 		}
 	}
+	/**
+	 * 
+	 * @param operando1 Valor que representa el primer operando
+	 * @return Valor float 
+	 */
 	public float raiz_cuadrada(float operando1)
 	{
 		if(operando1>=1)
@@ -49,6 +84,10 @@ public class Calcu
 		}
 		return -1;
 	}
+	/**
+	 * 
+	 * @param args metodo main
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
